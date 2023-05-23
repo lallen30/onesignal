1ddc843c8908c477ec09bd58863a8840d94b7869
Larry Bluestoneapps <larry@bluestoneapps.com>
2023-05-23 12:24:40 -0400

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scruffy.oneSignal',
+  appName: 'oneSignal',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import OneSignal from 'onesignal-cordova-plugin';
+
+// Call this function when your app starts
+function OneSignalInit(): void {
+  // Uncomment to set OneSignal device logging to VERBOSE  
+  // OneSignal.setLogLevel(6, 0);
+
+  // NOTE: Update the setAppId value below with your OneSignal AppId.
+  OneSignal.setAppId("6b79dcc6-93fa-45ab-af96-eaed3a6fd091");
+  OneSignal.setNotificationOpenedHandler(function (jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  });
+
+  // Prompts the user for notification permissions.
+  //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
+  OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+
+export class AppComponent {
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      OneSignalInit();
+    });
+  }
+}
